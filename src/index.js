d3488c23f4578c3d0f8601bd0799b7ff565239b1
angelicamcs12 <angelicamariachs@gmail.com>
2022-07-25 11:47:53 -0500

    Prototipo aun sin codificar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
+const li = document.querySelectorAll('.li');
+const bloque = document.querySelectorAll('.bloque');
+
+//Asignamos un click a todos los li 
+ li.forEach(( cadaLi , i )=>{
+    li[i].addEventListener('click',()=>{
+
+         //Quitamos la clase activo de li y bloque
+        li.forEach( ( cadaLi , i )=>{
+            li[i].classList.remove('activo')
+            bloque[i].classList.remove('activo')
+        })
+
+        //Ponemos la clase activo de li y bloque
+        li[i].classList.add('activo')
+        bloque[i].classList.add('activo')
+    })
+ })
+
+
 console.log(cipher);
